c5bc813e5aa8c53dfd8e93a723878ca022cbe3d1
Emanuel-Dan Acalfoaie <91905169+TheDarkestNightRises@users.noreply.github.com>
2024-03-03 14:10:04 +0100

    Value object implementation
    Co-Authored-By: Cosmin-Mihai Teodoru <93667171+Cosmin-Teodoru@users.noreply.github.com>
    Co-Authored-By: Andrei-George Lichi <93666980+Andrei-Lichi@users.noreply.github.com>
    Co-Authored-By: Oriana <85575367+Oriana11@users.noreply.github.com>

---
+++ src/Core/ViaEventAssociation.Core.Domain/Common/Bases/ValueObject.cs
@@ -0,0 +1,37 @@
+﻿namespace ViaEventAssociation.Core.Domain;
+
+public abstract class ValueObject
+{
+    public abstract IEnumerable<object> GetEqualityObjects();
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is null) return false;
+        if (obj.GetType() != GetType()) return false;
+
+        return ValuesAreEquals((ValueObject)obj);
+    }
+
+    private bool ValuesAreEquals(ValueObject other)
+    {
+        return GetEqualityObjects().SequenceEqual(other.GetEqualityObjects());
+    }
+
+    public override int GetHashCode()
+    {
+        return GetEqualityObjects()
+                .Aggregate(default(int), HashCode.Combine);
+    }
+
+    public static bool operator ==(ValueObject? left, ValueObject? right)
+    {
+        return left is not null && right is not null && Equals(right, left);
+    }
+
+
+    public static bool operator !=(ValueObject? left, ValueObject? right)
+    {
+        return left is not null && right is not null && !Equals(right, left);
+    }
+
+}

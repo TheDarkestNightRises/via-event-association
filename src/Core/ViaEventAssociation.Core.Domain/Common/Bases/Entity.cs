f3201a3457cf41751fabc3c2b0e4cea243a485c4
Emanuel-Dan Acalfoaie <91905169+TheDarkestNightRises@users.noreply.github.com>
2024-03-03 14:10:10 +0100

    Create Entity.cs
    Co-Authored-By: Cosmin-Mihai Teodoru <93667171+Cosmin-Teodoru@users.noreply.github.com>
    Co-Authored-By: Andrei-George Lichi <93666980+Andrei-Lichi@users.noreply.github.com>
    Co-Authored-By: Oriana <85575367+Oriana11@users.noreply.github.com>

---
+++ src/Core/ViaEventAssociation.Core.Domain/Common/Bases/Entity.cs
@@ -0,0 +1,37 @@
+﻿namespace ViaEventAssociation.Core.Domain;
+
+public abstract class Entity<TId>
+{
+    public TId Id { get; }
+
+    public Entity(TId id)
+    {
+        Id = id;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is null) return false;
+        if (obj.GetType() != GetType()) return false;
+
+        Entity<TId> otherEntity = (Entity<TId>) obj;
+        return otherEntity.Id!.Equals(Id);
+    }
+
+
+    public override int GetHashCode()
+    {
+        return Id!.GetHashCode();
+    }
+
+    public static bool operator ==(Entity<TId>? left, Entity<TId>? right)
+    {
+        return left is not null && right is not null && Equals(right, left);
+    }
+
+
+    public static bool operator !=(Entity<TId>? left, Entity<TId>? right)
+    {
+        return left is not null && right is not null && !Equals(right, left);
+    }
+}

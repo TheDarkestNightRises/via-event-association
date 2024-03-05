51901b4de6d728c81fc2a1c93c0f29b25eb837d5
Andrei-George Lichi <lichiag21@gmail.com>
2024-03-05 16:38:25 +0100

    Set number of guests unit tests added
    Co-Authored-By: Cosmin-Mihai Teodoru <93667171+Cosmin-Teodoru@users.noreply.github.com>
    Co-Authored-By: Emanuel-Dan Acalfoaie <91905169+TheDarkestNightRises@users.noreply.github.com>
    Co-Authored-By: Oriana <85575367+Oriana11@users.noreply.github.com>

---
+++ UnitTests/Features/Event/SetNumberOfGuests/SetNumberOfGuestsUnitTests.cs
@@ -0,0 +1,16 @@
+﻿using ViaEventAssociation.Core.Domain.Aggregates.Event.Values;
+
+namespace UnitTests.Features.Event.SetNumberOfGuests;
+
+public class SetNumberOfGuestsUnitTests
+{
+    [Fact]
+    public void GivenEvent_AndStatusIsDraft_WhenNumberOfGuestsIsSet_AndNumberIsLessThan50_ThenSetSelectedValue()
+    {
+        var eventAggregate = EventFactory.Init()
+            .WithStatus(EventStatus.Draft)
+            .Build();
+        eventAggregate.SetNumberOfGuest();
+        Assert.Equal(EventVisibility.Public, eventAggregate.EventVisibility);
+    }
+}

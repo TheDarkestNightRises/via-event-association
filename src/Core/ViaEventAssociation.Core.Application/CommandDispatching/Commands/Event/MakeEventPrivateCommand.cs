c29694bfa247acdc6d0164bddac7fd6184deacf1
Andrei-George Lichi <lichiag21@gmail.com>
2024-04-02 00:50:02 +0200

    Make event private command & handler

---
+++ src/Core/ViaEventAssociation.Core.Application/CommandDispatching/Commands/Event/MakeEventPrivateCommand.cs
@@ -0,0 +1,25 @@
+﻿using ViaEventAssociation.Core.Domain.Aggregates.Event.Values;
+using ViaEventAssociation.Core.Tools.OperationResult;
+
+namespace ViaEventAssociation.Core.Application.CommandDispatching.Commands.Event;
+
+public class MakeEventPrivateCommand
+{
+    public EventId Id { get; }
+    private MakeEventPrivateCommand(EventId id)
+    {
+        Id = id;
+    }
+    public static Result<MakeEventPrivateCommand> Create(string id)
+    {
+        var idResult = EventId.FromString(id);
+
+        if (idResult.IsFailure)
+        {
+            return idResult.Errors;
+        }
+        
+        return new MakeEventPrivateCommand(idResult.PayLoad);
+    }
+    
+}

6a9f4b4653d2df5d2633563dd0a036429e2805c4
Andrei-George Lichi <lichiag21@gmail.com>
2024-04-02 01:21:39 +0200

    Make event ready command & handler

---
+++ UnitTests/Features/Event/MakeEventReady/MakeEventReadyCommandHandlerTests.cs
@@ -0,0 +1,79 @@
+﻿using FakeItEasy;
+using Microsoft.Extensions.Time.Testing;
+using ViaEventAssociation.Core.Application.CommandDispatching.Commands;
+using ViaEventAssociation.Core.Application.CommandDispatching.Commands.Event;
+using ViaEventAssociation.Core.Application.Features.Event;
+using ViaEventAssociation.Core.Domain.Aggregates.Event.EventErrors;
+using ViaEventAssociation.Core.Domain.Aggregates.Event.Repository;
+using ViaEventAssociation.Core.Domain.Aggregates.Event.Values;
+using ViaEventAssociation.Core.Domain.Common.UnitOfWork;
+
+namespace UnitTests.Features.Event.MakeEventReady;
+
+public class MakeEventReadyCommandHandlerTests
+{
+    private IEventRepository _repositoryMock;
+    private IUnitOfWork _uowMock;
+    private Guid _eventId;
+    private readonly MakeEventReadyCommand cmd;
+    private readonly ICommandHandler<MakeEventReadyCommand> handler;
+    private readonly EventStatus _eventStatus;
+
+    public MakeEventReadyCommandHandlerTests()
+    {
+        _repositoryMock = A.Fake<IEventRepository>();
+        _uowMock = A.Fake<IUnitOfWork>();
+        _eventId = Guid.NewGuid();
+        cmd = MakeEventReadyCommand.Create(_eventId.ToString()).PayLoad;
+        handler = new MakeEventReadyCommandHandler(_repositoryMock, _uowMock);
+        _eventStatus = EventStatus.Ready;
+    }
+    
+    [Fact]
+    public async Task GivenValidCommand_WhenHandleAsync_ThenSuccess()
+    {
+        var timeProvider = new FakeTimeProvider(new DateTime(2023,7,20,19,0,0));
+        var originalEvent = EventFactory.Init()
+            .WithStatus(EventStatus.Draft)
+            .WithTitle(EventTitle.Create("Another title").PayLoad)
+            .WithDescription(EventDescription.Create("Description").PayLoad)
+            .WithVisibility(EventVisibility.Public)
+            .WithCapacity(EventCapacity.Create(25).PayLoad)
+            .WithTimeInterval(EventTimeInterval.Create(
+                new DateTime(2023,8,20,19,0,0), 
+                new DateTime(2023,8,20,21,0,0),
+                timeProvider).PayLoad)
+            .Build();
+        A.CallTo(() => _repositoryMock.GetAsync(cmd.Id)).Returns(originalEvent); 
+
+        // Act
+        var result = await handler.HandleAsync(cmd);
+
+        // Assert
+        Assert.True(result.IsSuccess);
+        Assert.Equal(_eventStatus,  originalEvent.EventStatus); 
+    }
+    
+    [Fact]
+    public async Task GivenInvalidCommand_WhenHandleAsync_ThenReturnFailure()
+    {
+        var timeProvider = new FakeTimeProvider(new DateTime(2023,7,20,19,0,0));
+        var originalEvent = EventFactory.Init()
+            .WithStatus(EventStatus.Draft)
+            .WithTitle(EventTitle.Create("Another title").PayLoad)
+            .WithDescription(EventDescription.Create("Description").PayLoad)
+            .WithVisibility(EventVisibility.None)
+            .WithCapacity(EventCapacity.Create(25).PayLoad)
+            .WithTimeInterval(EventTimeInterval.Create(
+                new DateTime(2023,8,20,19,0,0), 
+                new DateTime(2023,8,20,21,0,0),
+                timeProvider).PayLoad)
+            .Build();
+        A.CallTo(() => _repositoryMock.GetAsync(cmd.Id)).Returns(originalEvent); 
+        
+        var result = await handler.HandleAsync(cmd);
+        
+        Assert.True(result.IsFailure);
+        Assert.Contains(result.Errors, error => error == EventAggregateErrors.CanNotReadyAnEventWithNoVisibility);
+    }
+}
---
+++ UnitTests/Features/Event/MakeEventReady/MakeEventReadyCommandTests.cs
@@ -0,0 +1,14 @@
+﻿using ViaEventAssociation.Core.Application.CommandDispatching.Commands.Event;
+
+namespace UnitTests.Features.Event.MakeEventReady;
+
+public class MakeEventReadyCommandTests
+{
+    [Fact]
+    public void GivenNothing_WhenCreatingCommand_Success()
+    {
+        var guid = Guid.NewGuid().ToString();
+        var result = MakeEventReadyCommand.Create(guid);
+        Assert.True(result.IsSuccess);
+    }
+}

5da2a13b762f3b71f2506cd350331facc94ad2ae
Cosmin-Teodoru <93667171+Cosmin-Teodoru@users.noreply.github.com>
2024-04-02 00:11:09 +0200

    Update start and end time command & handler & tests

---
+++ UnitTests/Features/Event/UpdateStartAndEndTime/UpdateStartAndEndTimeCommandTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Time.Testing;
+using ViaEventAssociation.Core.Application.CommandDispatching.Commands.Event;
+using ViaEventAssociation.Core.Domain.Aggregates.Event.EventErrors;
+using ViaEventAssociation.Core.Domain.Aggregates.Event.Values;
+
+namespace UnitTests.Features.Event.UpdateStartAndEndTime;
+
+public class UpdateStartAndEndTimeCommandTests
+{
+    
+    [Fact]
+    public void Create_ValidInput_ReturnsSuccess()
+    {
+        // Arrange
+        EventId evId = EventId.Create();
+        DateTime start = DateTime.UtcNow.AddHours(1); //TODO: switch to actual dates when time provider fixed
+        DateTime end = start.AddHours(2);
+        
+        // Act
+        var result = UpdateTimeIntervalCommand.Create(evId.Id.ToString(), start, end);
+        
+        // Assert
+        Assert.True(result.IsSuccess);
+        Assert.Equal(evId, result.PayLoad.Id);
+        Assert.Equal(start, result.PayLoad.EventTimeInterval.Start);
+        Assert.Equal(end, result.PayLoad.EventTimeInterval.End);
+    }
+    [Fact]
+    public void Create_InvalidInput_ReturnsFailure()
+    {
+        // Arrange
+        EventId evId = EventId.Create();
+        DateTime start = DateTime.UtcNow.AddHours(2);//TODO: switch to actual dates when time provider fixed
+        DateTime end = start.AddHours(-1);
+        
+        // Act
+        var result = UpdateTimeIntervalCommand.Create(evId.Id.ToString(), start, end);
+        
+        // Assert
+        Assert.True(result.IsFailure);
+        Assert.Equal(EventAggregateErrors.EndTimeBeforeStartTime, result.Errors[0]);
+    }
+}
---
+++ src/Core/ViaEventAssociation.Core.Application/CommandDispatching/Commands/Event/UpdateTimeIntervalCommand.cs
@@ -0,0 +1,26 @@
+﻿using ViaEventAssociation.Core.Domain.Aggregates.Event.Values;
+using ViaEventAssociation.Core.Tools.OperationResult;
+
+namespace ViaEventAssociation.Core.Application.CommandDispatching.Commands.Event;
+
+public class UpdateTimeIntervalCommand
+{
+    public EventId Id { get; }
+    public EventTimeInterval EventTimeInterval { get; }
+    private UpdateTimeIntervalCommand(EventId id, EventTimeInterval interval) =>
+        (Id, EventTimeInterval) = (id, interval);
+
+    public static Result<UpdateTimeIntervalCommand> Create(string id, DateTime start, DateTime end,
+        TimeProvider? timeProvider = null)
+    {
+        Result<EventId> idResult = EventId.FromString(id);
+        if (idResult.IsFailure)
+            return idResult.Errors;
+        
+        Result<EventTimeInterval> timeIntervalResult = EventTimeInterval.Create(start, end, timeProvider); //TODO: remove time provider when implementation fixed
+        if (timeIntervalResult.IsFailure)
+            return timeIntervalResult.Errors;
+        
+        return new UpdateTimeIntervalCommand(idResult.PayLoad, timeIntervalResult.PayLoad);
+    }
+}

39550a5045d6b1eae77f4816eb2ede5a5e301458
Emanuel-Dan Acalfoaie <91905169+TheDarkestNightRises@users.noreply.github.com>
2024-04-02 00:30:49 +0200

    Guest aggregate create

--- UnitTests/Features/Event/UpdateEventDescription/UpdateEventDescriptionHandlerTests.cs
+++ UnitTests/Features/Event/UpdateEventDescription/UpdateEventDescriptionHandlerTests.cs
@@ -11,49 +11,47 @@ namespace UnitTests.Features.Event.UpdateEventDescription;
 
 public class UpdateEventDescriptionHandlerTests
 {
-    private IEventRepository _repositoryMock;
-    private IUnitOfWork _uowMock;
-    private Guid _eventId;
+    private readonly IEventRepository _repositoryMock;
+    private readonly IUnitOfWork _uowMock;
+    private readonly Guid _eventId;
+    private readonly UpdateEventDescriptionCommand cmd;
+    private readonly ICommandHandler<UpdateEventDescriptionCommand> handler;
+    private readonly string description;
 
     public UpdateEventDescriptionHandlerTests()
     {
         _repositoryMock = A.Fake<IEventRepository>();
         _uowMock = A.Fake<IUnitOfWork>();
         _eventId = Guid.NewGuid();
+        description = "Updated description";
+        cmd = UpdateEventDescriptionCommand.Create(_eventId.ToString(), description).PayLoad;
+        handler = new UpdateEventDescriptionCommandHandler(_repositoryMock, _uowMock);
     }
 
     [Fact]
     public async Task GivenValidCommand_WhenHandleAsync_ThenSuccess()
     {
         // Arrange
-        var updatedDescription = "Updated description";
-        var command = UpdateEventDescriptionCommand.Create(_eventId.ToString(), updatedDescription).PayLoad;
-        
         var originalEvent = EventFactory.ValidEvent();
-        A.CallTo(() => _repositoryMock.GetAsync(command.Id)).Returns(originalEvent); 
-        var handler = new UpdateEventDescriptionCommandHandler(_repositoryMock, _uowMock);
+        A.CallTo(() => _repositoryMock.GetAsync(cmd.Id)).Returns(originalEvent); 
 
         // Act
-        var result = await handler.HandleAsync(command);
+        var result = await handler.HandleAsync(cmd);
 
         // Assert
         Assert.True(result.IsSuccess);
-        Assert.Equal(updatedDescription, (string) originalEvent.EventDescription); 
+        Assert.Equal(description, (string) originalEvent.EventDescription); 
     }
     
     [Fact]
     public async Task GivenInvalidCommand_WhenHandleAsync_ThenReturnFailure()
     {
         // Arrange
-        var updatedDescription = "Updated description";
-        var command = UpdateEventDescriptionCommand.Create(_eventId.ToString(), updatedDescription).PayLoad;
-        
         var originalEvent = EventFactory.CanceledEvent();
-        A.CallTo(() => _repositoryMock.GetAsync(command.Id)).Returns(originalEvent); 
-        var handler = new UpdateEventDescriptionCommandHandler(_repositoryMock, _uowMock);
+        A.CallTo(() => _repositoryMock.GetAsync(cmd.Id)).Returns(originalEvent); 
 
         // Act
-        var result = await handler.HandleAsync(command);
+        var result = await handler.HandleAsync(cmd);
 
         // Assert
         Assert.True(result.IsFailure);

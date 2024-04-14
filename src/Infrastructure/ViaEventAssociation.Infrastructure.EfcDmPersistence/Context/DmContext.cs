d1bfc5cab34b393685455005589ab6bf9dc70b77
Emanuel-Dan Acalfoaie <91905169+TheDarkestNightRises@users.noreply.github.com>
2024-04-14 12:59:15 +0200

    Setup ef core
    Co-Authored-By: Andrei-George Lichi <93666980+Andrei-Lichi@users.noreply.github.com>
    Co-Authored-By: Cosmin-Mihai Teodoru <93667171+Cosmin-Teodoru@users.noreply.github.com>
    Co-Authored-By: Oriana <85575367+Oriana11@users.noreply.github.com>

---
+++ src/Infrastructure/ViaEventAssociation.Infrastructure.EfcDmPersistence/Context/DmContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ViaEventAssociation.Core.Domain.Aggregates.Event;
+using ViaEventAssociation.Core.Domain.Aggregates.Guest;
+
+namespace ViaEventAssociation.Infrastructure.EfcDmPersistence.Context;
+
+public class DmContext(DbContextOptions options) : DbContext(options)
+{
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(DmContext).Assembly);
+    }
+
+    public DbSet<EventAggregate> Events => Set<EventAggregate>();
+    public DbSet<GuestAggregate> Guests => Set<GuestAggregate>();   
+}

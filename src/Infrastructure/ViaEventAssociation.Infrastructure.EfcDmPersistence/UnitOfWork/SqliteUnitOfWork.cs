2998d8122a17fe715b8ccead23762b13448d80fa
Andrei-George Lichi <lichiag21@gmail.com>
2024-04-14 13:12:17 +0200

    Sqlite unit of work implemented
    Co-Authored-By: Oriana <85575367+Oriana11@users.noreply.github.com>
    Co-Authored-By: Emanuel-Dan Acalfoaie <91905169+TheDarkestNightRises@users.noreply.github.com>
    Co-Authored-By: Cosmin-Mihai Teodoru <93667171+Cosmin-Teodoru@users.noreply.github.com>

---
+++ src/Infrastructure/ViaEventAssociation.Infrastructure.EfcDmPersistence/UnitOfWork/SqliteUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ViaEventAssociation.Core.Domain.Common.UnitOfWork;
+
+namespace ViaEventAssociation.Infrastructure.EfcDmPersistence.UnitOfWork;
+
+public class SqliteUnitOfWork(DbContext context) : IUnitOfWork
+{
+    public Task SaveChangesAsync() => context.SaveChangesAsync();
+}

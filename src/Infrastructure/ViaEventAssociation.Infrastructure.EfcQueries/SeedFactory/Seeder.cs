cc2b7b27426d3e453deabf095eecd1fea62899db
Emanuel-Dan Acalfoaie <91905169+TheDarkestNightRises@users.noreply.github.com>
2024-04-21 19:39:14 +0200

    Create Seeder.cs
    Co-Authored-By: Andrei-George Lichi <93666980+Andrei-Lichi@users.noreply.github.com>
    Co-Authored-By: Cosmin-Mihai Teodoru <93667171+Cosmin-Teodoru@users.noreply.github.com>
    Co-Authored-By: Oriana <85575367+Oriana11@users.noreply.github.com>

---
+++ src/Infrastructure/ViaEventAssociation.Infrastructure.EfcQueries/SeedFactory/Seeder.cs
@@ -0,0 +1,13 @@
+﻿namespace ViaEventAssociation.Infrastructure.EfcQueries.SeedFactory;
+
+public static class Seeder
+{
+    public static async Task<VeadatabaseProductionContext> Seed(this VeadatabaseProductionContext context)
+    {
+        await context.Guests.AddRangeAsync(GuestSeedFactory.CreateGuests());
+        await context.Events.AddRangeAsync(EventSeedFactory.CreateEvents());
+        await context.Invitations.AddRangeAsync(InvitationSeedFactory.CreateInvitations());
+        await context.SaveChangesAsync();
+        return context;
+    }
+}

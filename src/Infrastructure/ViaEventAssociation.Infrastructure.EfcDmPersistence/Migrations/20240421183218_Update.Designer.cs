22c154795ca5894a0b0446357ef2fb52c6085648
Cosmin-Teodoru <93667171+Cosmin-Teodoru@users.noreply.github.com>
2024-04-21 21:40:57 +0200

    Updated event configuration

--- src/Infrastructure/ViaEventAssociation.Infrastructure.EfcDmPersistence/Migrations/20240421183218_Update.Designer.cs
+++ src/Infrastructure/ViaEventAssociation.Infrastructure.EfcDmPersistence/Migrations/20240421183218_Update.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ViaEventAssociation.Infrastructure.EfcDmPersistence.Context;
 
@@ -11,9 +12,11 @@ using ViaEventAssociation.Infrastructure.EfcDmPersistence.Context;
 namespace ViaEventAssociation.Infrastructure.EfcDmPersistence.Migrations
 {
     [DbContext(typeof(DmContext))]
-    partial class DmContextModelSnapshot : ModelSnapshot
+    [Migration("20240421183218_Update")]
+    partial class Update
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.4");

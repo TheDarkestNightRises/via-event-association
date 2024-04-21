4ff37effc268f83f318e315a14fde6cdeba3d373
Cosmin-Teodoru <93667171+Cosmin-Teodoru@users.noreply.github.com>
2024-04-21 15:32:50 +0200

    EventRepository & tests setup

---
+++ IntegrationTests/Repositories/EventRepositoryTests.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ViaEventAssociation.Infrastructure.EfcDmPersistence.Context;
+using Xunit;
+
+namespace IntegrationTests.Repositories;
+
+public class EventRepositoryTestsFixture : IDisposable
+{
+    public DbContext Context { get; private set; }
+
+    public EventRepositoryTestsFixture()
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<DmContext>();
+        optionsBuilder.UseInMemoryDatabase(databaseName: "testDb");
+        Context = new DmContext(optionsBuilder.Options);
+    }
+
+    public void Dispose()
+    {
+        Context.Dispose();
+    }
+}
+
+[CollectionDefinition("Database Collection")]
+public class DatabaseCollection : ICollectionFixture<EventRepositoryTestsFixture>
+{
+   
+}
+
+[Collection("Database Collection")]
+public class EventRepositoryTests
+{
+    private readonly EventRepositoryTestsFixture _fixture;
+
+    public EventRepositoryTests(EventRepositoryTestsFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
+    [Fact]
+    public void TestMethod1()
+    {
+        // Use _fixture.Context for testing
+    }
+
+    [Fact]
+    public void TestMethod2()
+    {
+        // Use _fixture.Context for testing
+    }
+}

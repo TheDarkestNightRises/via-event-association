2b1d13245430f09c4ecabd8a23b96e7c7693a874
Cosmin-Teodoru <93667171+Cosmin-Teodoru@users.noreply.github.com>
2024-04-22 23:46:13 +0200

    PersonalProfile Page
    Co-Authored-By: Emanuel-Dan Acalfoaie <91905169+TheDarkestNightRises@users.noreply.github.com>
    Co-Authored-By: Oriana <85575367+Oriana11@users.noreply.github.com>
    Co-Authored-By: Andrei-George Lichi <93666980+Andrei-Lichi@users.noreply.github.com>

---
+++ IntegrationTests/Queries/PersonalProfileQueryTest.cs
@@ -0,0 +1,31 @@
+﻿using ViaEventAssociation.Core.QueryContracts.Queries;
+using ViaEventAssociation.Infrastructure.EfcQueries.Queries;
+using Xunit;
+
+namespace IntegrationTests.Queries;
+
+public class PersonalProfileQueryTest : IClassFixture<DataFixture>
+{
+    private readonly DataFixture _fixture;
+    private readonly PersonalProfilePageQueryHandler _queryHandler;
+
+    public PersonalProfileQueryTest(DataFixture fixture)
+    {
+        _fixture = fixture;
+        _queryHandler = new PersonalProfilePageQueryHandler(_fixture.Context);
+    }
+
+    
+    // Test id
+    [Fact]
+    public async Task GetAsync_GuestId_ReturnsGuest()
+    {
+        // Arrange
+        
+        // Act
+        var result = await _queryHandler.HandleAsync(new PersonalProfilePage.Query("34403069-d5a4-4c12-88e2-7ead0f377c81"));
+
+        // Assert
+        Assert.NotNull(result);
+    }
+}
---
+++ src/Core/ViaEventAssociation.Core.QueryContracts/Queries/PersonalProfilePage.cs
@@ -0,0 +1,22 @@
+﻿using ViaEventAssociation.Core.QueryContracts.Contract;
+
+namespace ViaEventAssociation.Core.QueryContracts.Queries;
+
+public class PersonalProfilePage
+{
+    public record Query(string GuestId, TimeProvider? TimeProvider = null) : IQuery<Answer>;
+
+    public record Answer(PersonalProfile PersonalProfile);
+
+    public record PersonalProfile(
+        string FullName,
+        string Email,
+        string ProfilePicUrl,
+        int UpcomingEventCount,
+        List<UpcomingEvents> UpcomingEvents,
+        List<PastEvents> PastEvents,
+        int PendingInvitationsCount);
+    
+    public record UpcomingEvents(string Title,int NumberOfParticipants, string Start);
+    public record PastEvents(string Title);
+}

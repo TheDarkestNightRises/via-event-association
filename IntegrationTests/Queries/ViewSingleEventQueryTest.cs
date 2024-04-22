d0a2a9517a79f303fe99224bae4d1d3d82642453
Andrei-George Lichi <lichiag21@gmail.com>
2024-04-22 22:57:06 +0200

    ViewSingleEvent page
    Co-Authored-By: Cosmin-Mihai Teodoru <93667171+Cosmin-Teodoru@users.noreply.github.com>
    Co-Authored-By: Oriana <85575367+Oriana11@users.noreply.github.com>
    Co-Authored-By: Emanuel-Dan Acalfoaie <91905169+TheDarkestNightRises@users.noreply.github.com>

---
+++ IntegrationTests/Queries/ViewSingleEventQueryTest.cs
@@ -0,0 +1,31 @@
+﻿using ViaEventAssociation.Core.QueryContracts.Queries;
+using ViaEventAssociation.Infrastructure.EfcQueries.Queries;
+using Xunit;
+
+namespace IntegrationTests.Queries;
+
+public class ViewSingleEventQueryTest : IClassFixture<DataFixture>
+{
+    private readonly DataFixture _fixture;
+    private readonly ViewSingleEventQueryHandler _queryHandler;
+
+    public ViewSingleEventQueryTest(DataFixture fixture)
+    {
+        _fixture = fixture;
+        _queryHandler = new ViewSingleEventQueryHandler(_fixture.Context);
+    }
+    
+    [Fact]
+    public async Task GivenEventPageQuery_WhenRequestingPageData_ReturnsNotNull()
+    {
+        // Arrange
+        var query = new ViewSingleEvent.Query("1");
+        
+        // Act
+        var result = await _queryHandler.HandleAsync(query);
+
+        // Assert
+        Assert.NotNull(result);
+    }
+
+}
--- src/Core/ViaEventAssociation.Core.QueryContracts/Queries/UpcomingEventPage.cs
+++ src/Core/ViaEventAssociation.Core.QueryContracts/Queries/UpcomingEventPage.cs
@@ -2,10 +2,10 @@ using ViaEventAssociation.Core.QueryContracts.Contract;
 
 namespace ViaEventAssociation.Core.QueryContracts.Queries;
 
-public class UpcomingEventPage
+public abstract class UpcomingEventPage
 {
     public record Query(int PageSize, int PageNumber, string? EventName) : IQuery<Answer>;
 
     public record Answer(List<EventOverview> EventOverviews);
-    public record EventOverview(string Title, string Description, string Start, string End, string Visibility, int MaxCapacity, int NumberOfPartcicipants);
+    public record EventOverview(string Title, string Description, string Start, string End, string Visibility, int MaxCapacity, int NumberOfParticipants);
 }

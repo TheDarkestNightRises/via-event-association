ce16fa1ffcb8d249adec775e2f262e1a4fa2ca82
Oriana11 <rubyjpy@gmail.com>
2024-05-01 20:37:59 +0200

    Create MakeEventReadyEndpoint.cs

---
+++ src/Presentation/ViaEventAssociation.Presentation.WebAPI/Endpoints/Event/MakeEventReadyEndpoint/MakeEventReadyEndpoint.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using ViaEventAssociation.Core.Application.CommandDispatching.Commands.Event;
+using ViaEventAssociation.Core.Application.CommandDispatching.Dispatcher;
+using ViaEventAssociation.Presentation.WebAPI.Common;
+
+namespace ViaEventAssociation.Presentation.WebAPI.Endpoints.Event.MakeEventReadyEndpoint;
+
+public class MakeEventReadyEndpoint(ICommandDispatcher dispatcher) : ApiEndpoint
+    .WithRequest<MakeEventReadyRequest>
+    .WithoutResponse
+{
+
+    [HttpPost("/events/{id}/make-ready")]
+    public override async Task<ActionResult> HandleAsync([FromRoute] MakeEventReadyRequest request)
+    {
+        var cmdResult = MakeEventReadyCommand.Create(request.Id);
+        if (cmdResult.IsFailure)
+        {
+            return BadRequest(cmdResult.Errors);
+        }
+        var result = await dispatcher.DispatchAsync(cmdResult.PayLoad);
+        return result.IsSuccess ? Ok() : BadRequest(result.Errors);
+    }
+}
+
+public class MakeEventReadyRequest
+{
+    [FromRoute] public string Id { get; }
+}

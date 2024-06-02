46eedd0055d8b3b7e23de493de4e00698b55c9e9
Andrei-George Lichi <lichiag21@gmail.com>
2024-06-02 02:49:18 +0200

    Guest accepts/declines invitation added

---
+++ UnitTests/Features/Event/GuestAcceptsInvitation/GuestAcceptsInvitationUnitTests.cs
@@ -0,0 +1,107 @@
+﻿using UnitTests.Features.Guest;
+using ViaEventAssociation.Core.Domain.Aggregates.Entity.Values;
+using ViaEventAssociation.Core.Domain.Aggregates.Event.Entities.InvitationEntity;
+using ViaEventAssociation.Core.Domain.Aggregates.Event.Entities.InvitationEntity.InvitationErrors;
+using ViaEventAssociation.Core.Domain.Aggregates.Event.Values;
+using Xunit.Abstractions;
+
+namespace UnitTests.Features.Event.GuestAcceptsInvitation;
+
+public class GuestAcceptsInvitationUnitTests
+{
+    private readonly ITestOutputHelper _testOutputHelper;
+
+    public GuestAcceptsInvitationUnitTests(ITestOutputHelper testOutputHelper)
+    {
+        _testOutputHelper = testOutputHelper;
+    }
+
+    // UC14.s1
+    [Fact]
+    public void GivenAnExistingEventWithId_AndARegisteredInvitedGuest_WhenGuestAcceptsInvitation_InvitationStatusIsSetToAccepted()
+    {
+        // Arrange
+        var eventAggregate = EventFactory.Init()
+            .WithStatus(EventStatus.Active)
+            .WithCapacity(new EventCapacity(3))
+            .Build();
+
+        var registeredGuest = GuestFactory.Init()
+            .Build();
+
+        // Act
+        eventAggregate.InviteGuestToEvent(registeredGuest.Id);
+        eventAggregate.GuestAcceptsInvitation(registeredGuest.Id);
+        
+        // Assert
+        Assert.Equal(InvitationStatus.Accepted, eventAggregate.Invitations[0].InvitationStatus);
+    }
+    
+    // UC14.f1
+    [Fact]
+    public void GivenAnExistingEventWithId_AndARegisteredNotInvitedGuest_WhenGuestAcceptsInvitation_DisplayGuestHasNotBeenInvited()
+    {
+        // Arrange
+        var eventAggregate = EventFactory.Init()
+            .WithStatus(EventStatus.Active)
+            .WithCapacity(new EventCapacity(3))
+            .Build();
+
+        var registeredGuest = GuestFactory.Init()
+            .Build();
+
+        // Act
+        var result = eventAggregate.GuestAcceptsInvitation(registeredGuest.Id);
+        
+        // Assert
+        Assert.Equal(InvitationErrors.Invitation.GuestHasNotBeenInvitedToTheEvent, result.Errors.FirstOrDefault());    
+    }
+    
+    // UC14.f2
+    [Fact]
+    public void GivenAnExistingEventWithId_AndARegisteredInvitedGuest_AndCapacityIsFull_WhenGuestAcceptsInvitation_DisplayCapacityIsFull()
+    {
+        // Arrange
+        var eventAggregate = EventFactory.Init()
+            .WithStatus(EventStatus.Active)
+            .WithCapacity(new EventCapacity(1))
+            .Build();
+
+        var registeredGuest = GuestFactory.Init()
+            .Build();
+        
+        var registeredGuest2 = GuestFactory.Init()
+            .Build();
+        
+        // Act
+        eventAggregate.InviteGuestToEvent(registeredGuest.Id);
+        eventAggregate.InviteGuestToEvent(registeredGuest2.Id);
+        eventAggregate.GuestAcceptsInvitation(registeredGuest2.Id);
+
+        var result = eventAggregate.GuestAcceptsInvitation(registeredGuest.Id);
+        
+        // Assert
+        Assert.Equal(InvitationErrors.Invitation.EventShouldNotBeFullToAcceptInvitation, result.Errors.FirstOrDefault());    
+    }
+    
+    // UC14.f3
+    [Fact]
+    public void GivenAnExistingEventWithId_AndARegisteredInvitedGuest_AndEventIsNotActive_WhenGuestAcceptsInvitation_DisplayEventMustBeActiveToAccept()
+    {
+        // Arrange
+        var eventAggregate = EventFactory.Init()
+            .WithStatus(EventStatus.Ready)
+            .WithCapacity(new EventCapacity(1))
+            .Build();
+
+        var registeredGuest = GuestFactory.Init()
+            .Build();
+        
+        // Act
+        eventAggregate.InviteGuestToEvent(registeredGuest.Id);
+        var result = eventAggregate.GuestAcceptsInvitation(registeredGuest.Id);
+        
+        // Assert
+        Assert.Equal(InvitationErrors.Invitation.EventMustBeActiveToAcceptInvitation, result.Errors.FirstOrDefault());    
+    }
+}
---
+++ UnitTests/Features/Event/GuestDeclinesInvitation/GuestDeclinesInvitationUnitTests.cs
@@ -0,0 +1,72 @@
+﻿using UnitTests.Features.Guest;
+using ViaEventAssociation.Core.Domain.Aggregates.Entity.Values;
+using ViaEventAssociation.Core.Domain.Aggregates.Event.Entities.InvitationEntity.InvitationErrors;
+using ViaEventAssociation.Core.Domain.Aggregates.Event.Values;
+
+namespace UnitTests.Features.Event.GuestDeclinesInvitation;
+
+public class GuestDeclinesInvitationUnitTests
+{
+    // UC15.s1
+    [Fact]
+    public void GivenAnExistingEventWithId_AndARegisteredInvitedGuest_WhenGuestDeclinesInvitation_InvitationStatusIsSetToDeclined()
+    {
+        // Arrange
+        var eventAggregate = EventFactory.Init()
+            .WithStatus(EventStatus.Ready)
+            .WithCapacity(new EventCapacity(1))
+            .Build();
+
+        var registeredGuest = GuestFactory.Init()
+            .Build();
+        
+        // Act
+        eventAggregate.InviteGuestToEvent(registeredGuest.Id);
+        eventAggregate.GuestDeclinesInvitation(registeredGuest.Id);
+        
+        // Assert
+        Assert.Equal(InvitationStatus.Declined, eventAggregate.Invitations[0].InvitationStatus);
+    }
+    
+    // UC15.s2
+    [Fact]
+    public void GivenAnExistingEventWithId_AndARegisteredInvitedGuest_WhenGuestDeclinesInvitation_ButInvitationIsAccepted_InvitationStatusIsSetToDeclined()
+    {
+        // Arrange
+        var eventAggregate = EventFactory.Init()
+            .WithStatus(EventStatus.Ready)
+            .WithCapacity(new EventCapacity(1))
+            .Build();
+
+        var registeredGuest = GuestFactory.Init()
+            .Build();
+        
+        // Act
+        eventAggregate.InviteGuestToEvent(registeredGuest.Id);
+        eventAggregate.GuestAcceptsInvitation(registeredGuest.Id);
+        eventAggregate.GuestDeclinesInvitation(registeredGuest.Id);
+        
+        // Assert
+        Assert.Equal(InvitationStatus.Declined, eventAggregate.Invitations[0].InvitationStatus);
+    }
+    
+    // UC15.f1
+    [Fact]
+    public void GivenAnExistingEventWithId_AndARegisteredNotInvitedGuest_WhenGuestDeclinesInvitation_DisplayGuestHasNotBeenInvited()
+    {
+        // Arrange
+        var eventAggregate = EventFactory.Init()
+            .WithStatus(EventStatus.Ready)
+            .WithCapacity(new EventCapacity(1))
+            .Build();
+
+        var registeredGuest = GuestFactory.Init()
+            .Build();
+        
+        // Act
+        var result = eventAggregate.GuestDeclinesInvitation(registeredGuest.Id);
+        
+        // Assert
+        Assert.Equal(InvitationErrors.Invitation.GuestHasNotBeenInvitedToTheEvent, result.Errors.FirstOrDefault());    
+    }
+}
